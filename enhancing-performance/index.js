62dec13c96e662ddd6044331b1292fab7f7ccb0b
Nate Olson <nolson@rehearsal.com>
2018-07-29 23:10:27 -0700

    Use pm2 for cluster mode

--- enhancing-performance/index.js
+++ enhancing-performance/index.js
@@ -1,33 +1,21 @@
-// Imports.
-process.env.UV_THREADPOOL_SIZE = 1;
-const cluster = require('cluster');
+// Worker mode.
+const crypto = require('crypto');
+const express = require('express');
+const app = express();
 
-// Is the file being executed in master mode?
-if (cluster.isMaster) {
-    // Cause index.js to be executed *again*
-    // in worker mode.
-    cluster.fork();
-    cluster.fork();
-} else {
-    // Worker mode.
-    const crypto = require('crypto');
-    const express = require('express');
-    const app = express();
-
-    // Routes.
-    app.get('/', (req, res) => {
-        // Call pbkdf2.
-        crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', () => {
-            res.send('Hi there');
-        });
+// Routes.
+app.get('/', (req, res) => {
+    // Call pbkdf2.
+    crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', () => {
+        res.send('Hi there');
     });
+});
 
-    app.get('/fast', (req, res) => {
-        res.send('That was fast!');
-    });
+app.get('/fast', (req, res) => {
+    res.send('That was fast!');
+});
 
-    // Listen.
-    app.listen('3000', () => {
-        console.log('Listening on port 3000');
-    });
-}
+// Listen.
+app.listen('3000', () => {
+    console.log('Listening on port 3000');
+});
